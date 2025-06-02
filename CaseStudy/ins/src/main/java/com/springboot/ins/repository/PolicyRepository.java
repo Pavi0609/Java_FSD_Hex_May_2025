ac21a58db1409242815de31a68b0029a6a66b556
Pavi0609 <146163484+Pavi0609@users.noreply.github.com>
2025-06-02 09:13:07 +0530

    Delete CaseStudy/ins directory

--- CaseStudy/ins/src/main/java/com/springboot/ins/repository/PolicyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.ins.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboot.ins.model.Policy;
-
-public interface PolicyRepository extends JpaRepository<Policy, Long> {
-
-}
